3e78a3aac8f1b966d1553cabc173a6d1af86af9e
pol.valls.ortiz <pol.valls.ortiz@est.fib.upc.edu>
2018-10-09 11:34:18 +0100

    Change of routes

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/Tasks',Tasks);
+app.use('/api/Tasks',Tasks);
 app.use('/', routes);
 app.use('/users', users);
 
