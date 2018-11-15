fc2467827b691b1c38a6a326de0f5efacd376c60
polvallsortiz <pol.valls.ortiz@est.fib.upc.edu>
2018-11-15 20:30:52 +0100

    Transition of computers

--- app/controllers/clusters.controller.js
+++ app/controllers/clusters.controller.js
@@ -1,5 +1,8 @@
 const Clusters = require('../models/clusters.model.js');
 const Cluster = require('../models/cluster.model.js');
+const Relation = require('../models/relation.model.js');
+
+var response  = [];
 
 exports.create = (req, res) => {
 
@@ -38,18 +41,61 @@ exports.findAll = (req, res) => {
 
 exports.findByUID = (req, res) => {
     var uid = req.params.userUID;
-    Clusters.findOne({},{}, { sort : { 'created_at' : -1}}, function (err, post) {
+    getData(uid,function(resp) {
+       res.send(resp);
+    });
+};
+
+function getData(uid,callback) {
+    Clusters.findOne().sort('-createdAt').exec(function (err,post){
         var clusters = new Array(post.clustersgroups);
         console.log(clusters);
+        var found = false;
         clusters[0].forEach(function (cluster) {{
             console.log(cluster);
             clusteruids = new Array(cluster.uids);
-            console.log(clusteruids)
-            clusteruids[0].forEach(function (uidcluster) {
-                if(uidcluster == uid) {
-                    res.send(clusteruids[0]);
-                }
-            });
+            console.log(clusteruids[0]);
+            if(clusteruids[0].includes(uid)) {
+                found = true;
+                clusteruids[0].forEach(function (uids) {
+                    if(uids != uid) {
+                        getRelation(uid,uids,function(relation) {
+                            response.push({
+                                "uid" : uids,
+                                "relation" : relation
+                            });
+                            res.send(response);
+                        });
+                    }
+                });
+                res.send(response);
+            }
         }});
+        if(!found) {
+            res.send([])
+        }
     });
-};
+}
+
+function getRelation(uid1,uid2,callback) {
+    Relation.find({
+            $or: [
+                { $and: [ {firebase_uid1: uid1}, {firebase_uid2: uid2} ] },
+                { $and: [ {firebase_uid1: uid2}, {firebase_uid2: uid1} ] }
+                ]},
+        function (err, relation) {
+            if (!relation) {
+                callback("")
+            }
+            callback(relation.status);
+        }).catch(err => {
+        if (err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Relations not found with id " + req.params.userUID
+            });
+        }
+        return res.status(500).send({
+            message: "Error retrieving Relations with id " + req.params.userUID
+        });
+    })
+}
--- app/models/cluster.model.js
+++ app/models/cluster.model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-var Cluster = new mongoose.Schema({uids: [Number]});
+var Cluster = new mongoose.Schema({uids: [String]});
 
 module.exports = mongoose.model('Cluster', Cluster);
